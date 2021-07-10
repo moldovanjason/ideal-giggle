1bc0b1805580597f1d360e99c5ce1d519771e419
Jason Moldovan <moldovanjason@gmail.com>
2021-07-10 03:34:11 -0400

    trying to figure out

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Clicker from './Components/Clicker';
-import FetchRandomUser from './Components/FetchRandomUser';
+// import Clicker from './Components/Clicker';
+// import FetchRandomUser from './Components/FetchRandomUser';
 import MovieList from './Components/MovieList';
 
 function App() {
--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -6,9 +6,9 @@ function MovieList() {
             <h1 className="title">Movie List</h1>
             <div className="form">
                 <label htmlFor="" className="label">Movie Name</label>
-                <input type="text" className="input" name="movieName"/>
+                <input type="text" className="input" placeholder="enter movie name..." name="movieName"/>
                 <label htmlFor="" className="label">Movie Review</label>
-                <input type="text" className="input" name="review" />
+                <input type="text" className="input" placeholder="enter movie review..." name="review" />
             </div>
         </div>
     )
