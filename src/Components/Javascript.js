fb14b44308723402a1b6b21b1df9accb95cb50e7
Jason Moldovan <moldovanjason@gmail.com>
2021-08-17 00:16:58 -0400

    ok

--- src/Components/Javascript.js
+++ src/Components/Javascript.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Javascript() {
     return (
         <div>
-            {for (i<0;i=100;i++) {
+            for (i<0;i=100;i++) {
                 console.log(i)
-            }}
+            }
         </div>
     )
 }
